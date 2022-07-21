4ca608f85f187460349cc10b2eec7947c55d0f44
gs <spgseng@gmail.com>
2022-07-21 16:36:56 +0800

    21-7-22

---
+++ app/src/main/java/com/example/prac21_7/MainActivity.kt
@@ -0,0 +1,38 @@
+package com.example.prac21_7
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import com.example.prac21_7.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    // in java Student s;  so in kotlin  var s:Student(this is data type)
+    // lateint means binding this variable which data type is ActivityMainBinding will be declare/inialize future .
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        var student = Student("pgs", "RIT", 18)
+
+        binding.btnSetValue.setOnClickListener() {
+//            binding.tvAge.text=student.age.toString()
+//            binding.pgs1 =student
+
+            student.age = 16
+
+            binding.apply() {
+                invalidateAll()
+            }
+            //if update any field in dataclass need force UI to update or else will not update
+//        }
+
+            binding.btnGetValue.setOnClickListener() {
+//            binding.tvAge.text=student.age.toString()
+                binding.pgs1 = student
+            }
+        }
+    }
+}
---
+++ app/src/main/java/com/example/prac21_7/Student.kt
@@ -0,0 +1,5 @@
+package com.example.prac21_7
+
+data class Student(var name:String="", var programme:String="" , var  age:Int=0)
+//Int I is uppercase (must) similar as set get this data class
+// 0 and "" is default if not pass in any  value
